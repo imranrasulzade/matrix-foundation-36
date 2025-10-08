f5cfd9ce574b91dbbe04428e42ea2fbad32d9429
imranrasulzade <imranrasulzade5@gmail.com>
2025-10-08 20:41:01 +0400

    main changes

--- src/Main.java
+++ src/Main.java
@@ -3,6 +3,7 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
 
+        System.out.println("sdcnbsmdc");
         System.out.println("Salammm");
         System.out.println("Hello");
     }
